4697d342ca4443755d23ec8c0d208bb7d3f9603d
Domenik Töfflinger <domenikt96@yahoo.de>
2020-07-28 11:56:20 +0200

    Update loader.component.ts

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -6,9 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./loader.component.scss'],
 })
 export class LoaderComponent implements OnInit {
-  @Input() isLoading = false;
-  @Input() message: string | undefined;
-
   constructor() {}
 
   ngOnInit() {}
