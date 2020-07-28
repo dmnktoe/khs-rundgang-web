262a36c857a9a1fc9fc460865d88f596e0c27cac
Domenik Töfflinger <domenikt96@yahoo.de>
2020-07-28 11:56:49 +0200

    adds: text seperator component

---
+++ src/app/shared/text-seperator/text-seperator.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-seperator',
+  templateUrl: './text-seperator.component.html',
+  styleUrls: ['./text-seperator.component.scss'],
+})
+export class TextSeperatorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
