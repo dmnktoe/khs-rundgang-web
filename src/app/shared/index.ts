fbe481b474b1772f026a1e1735138dc9862996d7
Domenik Töfflinger <domenikt96@yahoo.de>
2020-07-28 11:56:25 +0200

    Update index.ts

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,5 @@
 export * from './shared.module';
 export * from './loader/loader.component';
-export * from './current-track/current-track.component';
+export * from './live/live.component';
+export * from './text-seperator/text-seperator.component';
+export * from './player/player.component';
