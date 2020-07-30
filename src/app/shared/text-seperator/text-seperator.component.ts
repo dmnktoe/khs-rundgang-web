46d0282a1698bd5c560c728cdbd616879da8f899
Domenik Töfflinger <domenikt96@yahoo.de>
2020-07-30 02:04:55 +0200

    implement border possibility in textSeperator

--- src/app/shared/text-seperator/text-seperator.component.ts
+++ src/app/shared/text-seperator/text-seperator.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TextSeperatorComponent implements OnInit {
   @Input() text: string;
   @Input() isLink: boolean;
+  @Input() border: boolean;
   @Input() url: string;
 
   constructor() {}
