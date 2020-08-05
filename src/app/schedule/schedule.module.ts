f4ff14c099c52117746a1aa606812e71c8b52ff7
Domenik Töfflinger <domenikt96@yahoo.de>
2020-08-05 02:08:08 +0200

    remove blank lines schedule module

--- src/app/schedule/schedule.module.ts
+++ src/app/schedule/schedule.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
 import { Title } from '@angular/platform-browser';
-
 import { CoreModule } from '@app/core';
 import { SharedModule } from '@app/shared';
-
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
 import { ScheduleDayListComponent } from './schedule-day-list/schedule-day-list.component';
