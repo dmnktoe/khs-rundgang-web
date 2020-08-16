bad4cb07b4673be0f8630e25dca094dcb54dacb1
Domenik Töfflinger <domenikt96@yahoo.de>
2020-08-16 03:26:01 +0200

    blog module incl list, single and template components

---
+++ src/app/templates/blog/blog-single/blog-single.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { ApiService } from '@app/core/services/api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import Vibrant from 'node-vibrant';
+import { Palette } from 'node-vibrant/lib/color';
+
+@Component({
+  selector: 'app-blog-single',
+  templateUrl: './blog-single.component.html',
+  styleUrls: ['./blog-single.component.scss'],
+})
+export class BlogSingleComponent implements OnInit {
+  blogPost: any;
+  title: any;
+  hex: any;
+  rgba: any;
+  isLoading = false;
+  id: any;
+  message: string;
+  private sub: any;
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private titleService: Title
+  ) {}
+
+  public setTitle({ title }: { title: any }) {
+    this.titleService.setTitle('Radio Rasclat • ' + title);
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe((params) => {
+      this.isLoading = true;
+      this.id = params.id;
+      this.apiService
+        .getBlogPost({ blogPostId: this.id })
+        .pipe(
+          finalize(() => {
+            this.isLoading = false;
+          })
+        )
+        .subscribe((blogPost) => {
+          this.blogPost = blogPost;
+          this.setTitle({ title: this.blogPost.title });
+          Vibrant.from(
+            'https://cors-anywhere.herokuapp.com/' + this.blogPost.image
+          )
+            .getPalette()
+            .then((palette) => {
+              this.hex = palette.Vibrant.hex;
+              this.rgba =
+                'rgba(' +
+                palette.Vibrant.r +
+                ',' +
+                palette.Vibrant.g +
+                ',' +
+                palette.Vibrant.b +
+                ',0.15)';
+            });
+        });
+    });
+  }
+}
