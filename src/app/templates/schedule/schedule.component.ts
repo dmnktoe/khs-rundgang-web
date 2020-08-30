14cefd2f7974160624c91ec96610b2c598c50879
Domenik Töfflinger <domenikt@me.com>
2020-08-30 20:04:28 +0200

    revamped schedule list with current day indicator

--- src/app/templates/schedule/schedule.component.ts
+++ src/app/templates/schedule/schedule.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 import { ApiService } from '@app/core/services/api.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-schedule',
@@ -11,10 +12,17 @@ export class ScheduleComponent implements OnInit {
   schedule: any;
   isLoading = false;
   view = 'thisWeek';
+  todayDate: any;
+  thisWeekDays: any;
+  nextWeekDays: any;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.getTodayDate();
+    this.createThisWeekDates();
+    this.createNextWeekDates();
+
     this.isLoading = true;
     this.apiService
       .getSchedule()
@@ -26,5 +34,41 @@ export class ScheduleComponent implements OnInit {
       .subscribe((schedule) => {
         this.schedule = schedule;
       });
+  }
+
+  getTodayDate() {
+    this.todayDate = moment().startOf('day').toString();
+  }
+
+  createThisWeekDates() {
+    const startOfThisWeek = moment().startOf('isoWeek');
+    const endOfThisWeek = moment().endOf('isoWeek');
+
+    const thisWeekDays = [];
+    let day = startOfThisWeek;
+
+    while (day <= endOfThisWeek) {
+      thisWeekDays.push(day.toString());
+      day = day.clone().add(1, 'd');
+    }
+
+    this.thisWeekDays = thisWeekDays;
+    console.log(this.thisWeekDays[6]);
+    console.log(this.todayDate);
+  }
+
+  createNextWeekDates() {
+    const startOfNextWeek = moment().add(1, 'weeks').startOf('isoWeek');
+    const endOfNextWeek = moment().add(1, 'weeks').endOf('isoWeek');
+
+    const nextWeekDays = [];
+    let day = startOfNextWeek;
+
+    while (day <= endOfNextWeek) {
+      nextWeekDays.push(day.toDate());
+      day = day.clone().add(1, 'd');
+    }
+
+    this.nextWeekDays = nextWeekDays;
   }
 }
