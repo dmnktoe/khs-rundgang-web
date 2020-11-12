b463ff46b1044d9d9455fcf23ba78af8bd20fbef
Domenik Töfflinger <domenikt96@yahoo.de>
2020-11-12 13:28:47 +0100

    update cookie policy page

--- src/app/templates/cookies/cookies.component.ts
+++ src/app/templates/cookies/cookies.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cookies',
@@ -7,17 +6,7 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./cookies.component.scss'],
 })
 export class CookiesComponent implements OnInit {
-  constructor(
-    private renderer2: Renderer2,
-    @Inject(DOCUMENT) private document: { body: any }
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    const s = this.renderer2.createElement('script');
-    s.type = 'text/javascript';
-    s.src =
-      'https://consent.cookiebot.com/e9dc8e8a-a27e-4198-82ed-f69f7b80cd66/cd.js';
-    s.text = ``;
-    this.renderer2.appendChild(this.document.body, s);
-  }
+  ngOnInit() {}
 }
