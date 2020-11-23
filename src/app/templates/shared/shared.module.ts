d0e53bcd34e9fbb57d17304ea960ad2e05482e97
Domenik Töfflinger <domenikt96@yahoo.de>
2020-11-23 18:04:58 +0100

    Update shared.module.ts

--- src/app/templates/shared/shared.module.ts
+++ src/app/templates/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-import { ApiService } from '../../core/services/api.service';
-import { LimitPipe } from '../../core/pipes/LimitPipe';
-import { ReadingTimePipe } from '../../core/pipes/ReadingTimePipe';
-import { SafeHtmlPipe } from '../../core/pipes/SafeHtmlPipe';
-import { MomentPipe } from '../../core/pipes/MomentPipe';
+import { ApiService } from '@app/core/services/api.service';
+import { LimitPipe } from '@app/core/pipes/LimitPipe';
+import { ReadingTimePipe } from '@app/core/pipes/ReadingTimePipe';
+import { SafeHtmlPipe } from '@app/core/pipes/SafeHtmlPipe';
+import { MomentPipe } from '@app/core/pipes/MomentPipe';
 import { TimeagoModule } from 'ngx-timeago';
 import { PlayerComponent } from './player/player.component';
 import { RouterModule } from '@angular/router';
@@ -19,6 +19,7 @@ import { PageBannerComponent } from './page-banner/page-banner.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { TranslateModule } from '@ngx-translate/core';
 import { SocialIconsComponent } from './social-icons/social-icons.component';
+import { LanguageSelectComponent } from './language-select/language-select.component';
 
 @NgModule({
   imports: [
@@ -43,6 +44,7 @@ import { SocialIconsComponent } from './social-icons/social-icons.component';
     DarkModeToggleComponent,
     PageBannerComponent,
     SocialIconsComponent,
+    LanguageSelectComponent,
   ],
   exports: [
     LoaderComponent,
@@ -58,6 +60,7 @@ import { SocialIconsComponent } from './social-icons/social-icons.component';
     DarkModeToggleComponent,
     PageBannerComponent,
     SocialIconsComponent,
+    LanguageSelectComponent,
   ],
   providers: [ApiService],
 })
